a4d636a0be8fd61c95aa08a5e41e1824bf72c2d4
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-06-10 08:53:32 -0500

    database parametters

---
+++ api/settings/db_connect.php
@@ -0,0 +1,57 @@
+<?php
+
+require_once "./settings/DBvalues.php";
+
+class Connection
+{
+
+    public function connect()
+    {
+        $respuesta = "";
+        try {
+            //code... 
+            $enlace = new PDO(SGBD, USER, PASSWORD);
+            $respuesta = "conexion exitosa a la base de datos";
+        } catch (Exception $e) {
+            $respuesta = "Error: " . $e->getMessage(); // Captura más específica y mensaje detallado
+        }
+        return $respuesta;
+    }
+
+    protected function cleanString($cadena)
+    {
+        $valores_eliminar = array(
+            "/<script>/i",
+            "/<\/script>/i",
+            "/select \* from/i",
+            "/insert into/i",
+            "/delete from/i",
+            '/["\']/',
+            "/<script type/i",
+            "/<script src/i"
+        );
+        $cadena = trim($cadena);
+        $cadena = preg_replace($valores_eliminar, "", $cadena);
+        return $cadena;
+    }
+
+
+    protected function executeConsulta($sql)
+    {
+        $conexion = self::connect()->prepare($sql);
+        $conexion->execute();
+        $respuesta = $conexion->fetchAll(PDO::FETCH_ASSOC);
+        $conexion = null;
+        return $respuesta;
+    }
+
+    protected function executeSelect($tabla)
+    {
+        $conexion = self::connect()->prepare("SELECT * FROM " . $tabla);
+        $conexion->execute();
+        $respuesta = $conexion->fetchAll(PDO::FETCH_ASSOC);
+        $conexion = null;
+        return $respuesta;
+    }
+
+}
---
+++ api/settings/db_values.php
@@ -0,0 +1,7 @@
+<?php
+const SERVER="localhost";
+const DB="registro_personas";
+const USER="root";
+const PASSWORD="";
+
+const SGBD="mysql:host=".SERVER.";dbname=".DB;
