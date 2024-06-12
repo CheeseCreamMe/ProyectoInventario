184620a348772ea212511d8cb2f2b913e2a1ce42
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-06-12 09:37:57 -0500

    create controllers dor person and users

---
+++ api/controller/person_controller.php
@@ -0,0 +1,21 @@
+<?php
+class PersonController {
+
+    public function __construct() {}
+
+    public function getPerson(){}
+
+    public function getAllPersons(){}
+
+    public function getPersonById($id){}
+
+    public function getPersonsByName($name){}
+
+    public function getPersonsByLastname($lastname){}
+
+    public function setPerson(){}
+    
+    public function updatePerson($id){}
+
+    public function deletePersonById($id){}
+}    
---
+++ api/controller/usuario_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+class UsuarioController {
+
+    public function getUser(){}
+
+    public function getAllUsers(){}
+
+    public function getUserById(){}
+
+    public function getUsersByName(){}
+
+    public function getUsersByLastname(){}
+
+    public function setUser(){}
+
+    public function updateUserById(){}
+    
+    public function replacePassword(){}
+
+    public function deleteUserById() {}
+
+}    
