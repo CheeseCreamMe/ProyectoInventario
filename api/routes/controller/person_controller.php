533f17f2ca657e83fa219f04fff4d27da166b7bb
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-06-13 11:58:08 -0500

    add example for json data

--- api/routes/controller/person_controller.php
+++ api/routes/controller/person_controller.php
@@ -1,14 +1,39 @@
 <?php
-require_once "./routes/model/PersonDAO.php";
+require_once "./routes/model/person_dao.php";
 
 class PersonController extends PersonDAO {
+
+    /*
+    // yo lo uso como test de funcionabilidad
+    //ejmplo de los datos que debe recibir en formato json desde el front
+    public function get()
+    {
+        $data = json_decode('{
+            "name": "Juan",
+            "second_name": "Carlos",
+            "lastname": "Pérez",
+            "second_lastname": "González",
+            "edad": 30,
+            "estado": "Activo",
+            "fecha_ingreso": "2024-06-13",
+            "direccion": "Calle Falsa 123",
+            "telefono": "123456789",
+            "cargo": "Ingeniero"
+        }', true);
+
+        // Simular el POST llamando a la función createPerson con los datos decodificados
+        $_POST = $data;
+        $this->createPerson();
+    }
+        */
+
     public function createPerson() {
         $response = [
             'status' => 'error',
             'message' => 'Método no permitido'
         ];
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_POST['name'])) {  // Asegurar que el método POST simulado sea aceptado en el ejmplo de arriba
             $person = new PersonModel(
                 null,
                 $this->cleanString($_POST['name']),
@@ -30,7 +55,7 @@ class PersonController extends PersonDAO {
                 $response['message'] = 'Error al crear persona';
             }
         }
-
+        
         echo json_encode($response);
     }
 }
