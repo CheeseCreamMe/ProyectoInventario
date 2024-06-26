a334366dbc880a8b8f9a878aa7940db45dcdcafd
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-06-26 11:17:11 -0600

    change render template to data null

--- app/components/tablePerson.js
+++ app/components/tablePerson.js
@@ -1,47 +1,74 @@
-    const apiUrl = "http://localhost/proyecto/api/person/getAllPersons";
+const apiUrl = "http://localhost/proyecto/api/person/getAllPersons";
 
-    async function fetchData() {
-        try {
-            const response = await fetch(apiUrl);
-            const result = await response.json();
-            return result.data; // Ajustar de acuerdo con la estructura real de la respuesta
-        } catch (error) {
-            console.error("Error al obtener los datos:", error);
-        }
+async function fetchData() {
+    try {
+        const response = await fetch(apiUrl);
+        const result = await response.json();
+        return result.data; // Ajustar de acuerdo con la estructura real de la respuesta
+    } catch (error) {
+        console.error("Error al obtener los datos:", error);
+        return null;
     }
-     
-    function renderTemplate(data) {
-        const rows = data.map(person => `
+}
+
+function renderTemplate(data) {
+    if (!data || data.length === 0) {
+        return renderTemplateError();
+    }
+
+    const rows = data.map(person => `
+        <tr>
+            <td class="first_var">${person.name}</td>
+            <td>${person.secondName || ''}</td>
+            <td class="first_var">${person.lastname}</td>
+            <td>${person.secondLastname || ''}</td>
+            <td>${person.direccion || ''}</td>
+            <td>${person.telefono}</td>
+            <td>${person.estado}</td>
+            <td><img src="${person.imagen}" alt="Imagen de ${person.name}" width="50"></td>
+            <td class="last_var"><button type="button" value="${person.id}" id="editButton">Editar</button></td>
+        </tr>
+    `).join('');
+
+    return `
+        <table class="myTable">  
+            <caption class="tittle">Lista de Personas</caption>
             <tr>
-                <td class="firts_var">${person.name}</td>
-                <td>${person.secondName || ''}</td>
-                <td class="firts_var">${person.lastname}</td>
-                <td>${person.secondLastname || ''}</td>
-                <td>${person.direccion || ''}</td>
-                <td>${person.telefono}</td>
-                <td>${person.estado}</td>
-                <td class="last_var"><img src="${person.imagen}" alt="Imagen de ${person.name}" width="50"></td>
+                <th colspan="2">Nombre</th>
+                <th colspan="2">Apellido</th>
+                <th>Dirección</th>
+                <th>Teléfono</th>
+                <th>Estado</th>
+                <th>Imagen</th>
+                <th class="last_var">Acciones</th>
             </tr>
-        `).join('');
-        return `
-            <table class="myTable">  
-                <caption class="tittle">Lista de Personas</caption>
-                <tr>
-                    <th colspan="2">Nombre</th>
-                    <th colspan="2">Apellido</th>
-                    <th>Dirección</th>
-                    <th>Teléfono</th>
-                    <th>Estado</th>
-                    <th class="last_var">Imagen</th>
-                </tr>
-                ${rows}
-            </table>
-        `;
-    }
-    
-    export async function getTable() {
-        const data = await fetchData();
-        const app = document.getElementById('dataTable');
-        app.innerHTML = renderTemplate(data);
-    }
+            ${rows}
+        </table>
+    `;
+}
 
+function renderTemplateError() {
+    return `
+        <table class="myTable">  
+            <caption class="tittle">Lista de Personas</caption>
+            <tr>
+                <th colspan="2">Nombre</th>
+                <th colspan="2">Apellido</th>
+                <th>Dirección</th>
+                <th>Teléfono</th>
+                <th>Estado</th>
+                <th>Imagen</th>
+                <th class="last_var">Acciones</th>
+            </tr>
+            <tr>
+                <td colspan="9">No hay datos registrados para mostrar aquí</td>
+            </tr>
+        </table>
+    `;
+}
+
+export async function getTable() {
+    const data = await fetchData();
+    const app = document.getElementById('dataTable');
+    app.innerHTML = renderTemplate(data);
+}
