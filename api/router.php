e8f34b68836a87cdf4ff5503a17eb34c8f5a4ccb
CheeseCream <107474465+CheeseCreamMe@users.noreply.github.com>
2024-06-28 10:40:49 -0600

    adde control options in header

--- api/router.php
+++ api/router.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 $controllerName = isset($_GET['controller']) ? $_GET['controller'] : null;
 $methodName = isset($_GET['method']) ? $_GET['method'] : null;
 
